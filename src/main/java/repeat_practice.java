f64ba2fa32e9bb1d4e3e2bf57c93cb02e86b0dcd
olkasen765 <o.kassenov@eot.kz>
2024-10-17 10:15:03 -0500

    practice

--- src/main/java/repeat_practice.java
+++ src/main/java/repeat_practice.java
@@ -1,12 +1,28 @@
-public class repeat_practice {
+import java.util.Scanner;
+
+class Main {
+
     public static void main(String[] args) {
+        // Create a Scanner object to read input
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Hello");
+        // Read the integer
+        int age = sc.nextInt();
 
+        // Write your code here to determine the life stage based on the input age
+        // You should use if...else if...else statement
+        // Then, print out the appropriate message
 
+        if (age < 13) {
+            System.out.println("Child");
+        } else if (age > 12 && age < 18) {
+            System.out.println("Teenager");
+        } else if (age > 17 && age < 60) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("Citizen");
+        }
 
-
+        sc.close();
     }
-
-
 }
