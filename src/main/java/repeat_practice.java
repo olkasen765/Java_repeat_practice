de31f30682a41412ade7562881c61f63ca7ca321
olkasen765 <o.kassenov@eot.kz>
2024-12-03 20:57:33 -0600

    practice

--- src/main/java/repeat_practice.java
+++ src/main/java/repeat_practice.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
@@ -12,17 +13,33 @@ class Main {
 
         double Income = Bubblegum + Toffee + Ice_cream + Milk_chocolate + Doughnut + Pancake + Coffee;
 
-        System.out.println("Earned amount:");
-        System.out.println("Bubblegum: $" + Bubblegum);
-        System.out.println("Toffee: $" + Toffee);
-        System.out.println("Ice cream: $" + Ice_cream);
-        System.out.println("Milk chocolate: $" + Milk_chocolate);
-        System.out.println("Doughnut: $" + Doughnut);
-        System.out.println("Pancake: $" + Pancake);
-        System.out.println("Coffee: $" + Coffee);
+//        System.out.println("Earned amount:");
+//        System.out.println("Bubblegum: $" + Bubblegum);
+//        System.out.println("Toffee: $" + Toffee);
+//        System.out.println("Ice cream: $" + Ice_cream);
+//        System.out.println("Milk chocolate: $" + Milk_chocolate);
+//        System.out.println("Doughnut: $" + Doughnut);
+//        System.out.println("Pancake: $" + Pancake);
+//        System.out.println("Coffee: $" + Coffee);
         System.out.println("\nIncome: $" + Income);
         System.out.printf("Average per item: $" + "%.1f%n", Income/7);
 
+        String hello = "Hello, world. How are you?";
+        System.out.println(hello);
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter your full name: ");
+        String fullName = scan.nextLine();
+
+        System.out.println("Please enter your address: ");
+        String address = scan.nextLine();
+        System.out.println();
+
+        System.out.println("Full name: " + fullName);
+        System.out.println("Address: " + address);
+
+
+        scan.close();
     }
 }
